745bf54d31491821d56e7826127bf064ffbce8ec
Nilay Singh <snilay011@gmail.com>
2023-01-31 09:49:17 +0530

    API CoinGecko Setup Complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  
+  const [coins , setCoins] = useState();
+
+  const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+
+  useEffect(()=>{
+     axios.get(url).then((response)=>{
+       setCoins(response.data);
+     }).catch((err)=>{
+       console.log(err);
+     })
+  },[]);
+
+  console.log(coins);
+
   return (
     <div className="App">
       Hello
